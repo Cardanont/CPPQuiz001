09b0c85104155cd73de9157175dc6f8b69e55af3
Daniel Ontiveros <cardanont@gmail.com>
2019-06-22 02:00:33 -0500

    Starting the quiz

---
+++ Quiz.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room... \n";
+    std::cout << "You need to enter the correct codes to continue...";
+    return 0;
+}
